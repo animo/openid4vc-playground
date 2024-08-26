09615bb5740d95fecc54e7d899bb7c82142f106f
Timo Glastra <timo@animo.id>
2024-08-26 16:06:16 +0200

    update title
    Signed-off-by: Timo Glastra <timo@animo.id>

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'OpenID4VC Playground',
+  title: 'Animo Funke Playground',
   description: 'By Animo',
 }
 
